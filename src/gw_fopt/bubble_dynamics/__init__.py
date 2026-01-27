5e0c1cbd6655ad1efd2ae6cd51059a33c3cbe3d1
hoquoctrung <hoquoctrung98@gmail.com>
2026-01-27 11:27:02 +0100

    refactor: remove unnecessary stuffs

--- src/gw_fopt/bubble_dynamics/__init__.py
+++ src/gw_fopt/bubble_dynamics/__init__.py
@@ -1,9 +1,5 @@
 from . import bubble_simulator, visualizer, utils
 
-# from .bubble_simulator import *
-# from .visualizer import *
-# from .utils import *
-
 __all__ = [
     "bubble_simulator",
     "visualizer",
