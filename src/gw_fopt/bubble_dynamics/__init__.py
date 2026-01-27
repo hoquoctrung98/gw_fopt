158b003d9c69a79a1d85ba8fc0f8c5201b89176b
hoquoctrung <hoquoctrung98@gmail.com>
2026-01-27 10:13:54 +0100

    fix: wrong structure of bubble_dynamics due to legacy

---
+++ src/gw_fopt/bubble_dynamics/__init__.py
@@ -0,0 +1,11 @@
+from . import bubble_simulator, visualizer, utils
+
+# from .bubble_simulator import *
+# from .visualizer import *
+# from .utils import *
+
+__all__ = [
+    "bubble_simulator",
+    "visualizer",
+    "utils",
+]
--- src/gw_fopt/bubble_dynamics/src/gw_fopt/bubble_dynamics/__init__.py
+++
@@ -1,6 +0,0 @@
-from .bubble_simulator import *
-from .visualizer import *
-from .utils import *
-
-def hello() -> str:
-    return "Hello from pygw-fopt!"
